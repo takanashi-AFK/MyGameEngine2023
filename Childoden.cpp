3e3001dd6eb3e921dd0dd792e308e7d874c40ba5
陽悦 佐藤 <youetsux@gmail.com>
2023-07-12 12:19:31 +0900

    子オブジェクトを登録して親オブジェクトのワールドマトリクスを適用

---
+++ Childoden.cpp
@@ -0,0 +1,40 @@
+#include "Childoden.h"
+#include "Engine/Fbx.h"
+
+Childoden::Childoden(GameObject* parent)
+	:GameObject(parent, "ChildOden")
+{
+}
+
+Childoden::~Childoden()
+{
+}
+
+void Childoden::Initialize()
+{
+    pFbx = new Fbx;
+    pFbx->Load("Assets/oden.fbx");
+    transform_.scale_.x = 0.2f;
+    transform_.scale_.y = 0.2f;
+    transform_.scale_.z = 0.2f;
+    transform_.position_.x = 2.0f;
+    transform_.position_.y = 1.0f;
+}
+
+void Childoden::Update()
+{
+    transform_.rotate_.y++;
+    if (transform_.rotate_.y > 200)
+    {
+        KillMe();
+    }
+}
+
+void Childoden::Draw()
+{
+    pFbx->Draw(transform_);
+}
+
+void Childoden::Release()
+{
+}
--- Player.cpp
+++ Player.cpp
@@ -1,5 +1,7 @@
 #include "Player.h"
 #include "Engine/Fbx.h"
+#include "Childoden.h"
+#include "Engine/Input.h"
 
 Player::Player(GameObject* parent)
 	:GameObject(parent,"Player"), pFbx(nullptr)
@@ -18,11 +20,18 @@ void Player::Initialize()
 	this->transform_.scale_.x = 0.5;
 	this->transform_.scale_.y = 0.5;
 	this->transform_.scale_.z = 0.5;
+	Instantiate<Childoden>(this);
+	GameObject* pChildOden2 = Instantiate<Childoden>(this);
+	pChildOden2->SetPosition(-2, 1, 0);
 }
 
 void Player::Update()
 {
 	transform_.rotate_.y ++;
+	if (transform_.rotate_.y > 600)
+	{
+		KillMe();
+	}
 
 }
 
