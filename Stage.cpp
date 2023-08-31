92d486a72671f3e94f0f16505d4ccd8cd88ef8c4
youetsux <youetsux@gmail.com>
2023-08-31 17:23:06 +0900

    高さと、種類を設定できるようにした

--- Stage.cpp
+++ Stage.cpp
@@ -1,10 +1,28 @@
 #include "Stage.h"
 #include "Engine/Model.h"
 
+void Stage::SetBlock(int _x, int _z, BLOCKTYPE _type)
+{
+	table_[_x][_z].type = _type;
+}
+
+void Stage::SetBlockHeight(int _x, int _z, int _height)
+{
+	table_[_x][_z].height = _height;
+}
+
 //コンストラクタ
 Stage::Stage(GameObject* parent)
-    :GameObject(parent, "Stage"), hModel_(-1)
+	:GameObject(parent, "Stage")
 {
+	for (int i = 0; i < MODEL_NUM; i++) {
+		hModel_[i] = -1;
+	}
+	for (int z = 0; z < ZSIZE; z++) {
+		for (int x = 0; x < XSIZE; x++) {
+			SetBlock(x, z, DEFAULT);
+		}
+	}
 }
 
 //デストラクタ
@@ -15,9 +33,27 @@ Stage::~Stage()
 //初期化
 void Stage::Initialize()
 {
-    //モデルデータのロード
-    hModel_ = Model::Load("assets/BoxDefault.fbx");
-    assert(hModel_ >= 0);
+	string modelname[] = {
+		"BoxDefault.fbx",
+		"BoxBrick.fbx",
+		"BoxGrass.fbx",
+		"BoxSand.fbx",
+		"BoxWater.fbx"
+	};
+	string fname_base = "assets/";
+	//モデルデータのロード
+	for (int i = 0; i < MODEL_NUM; i++) {
+		hModel_[i] = Model::Load(fname_base + modelname[i]);
+		assert(hModel_[i] >= 0);
+	}
+	//tableにブロックのタイプをセットしてやろう！
+	for (int z = 0; z < ZSIZE; z++) {
+		for (int x = 0; x < XSIZE; x++) {
+			SetBlock(x, z, (BLOCKTYPE)(z%5));
+			SetBlockHeight(x, z, x % 5);
+		}
+	}
+
 }
 
 //更新
@@ -28,19 +64,26 @@ void Stage::Update()
 //描画
 void Stage::Draw()
 {
-    Model::SetTransform(hModel_, transform_);
-    Model::Draw(hModel_);
-    for (int x = 0; x < 15; x++)
-    {
-        for (int z = 0; z < 15; z++)
-        {
-            Transform trans;
-            trans.position_.x = x;
-            trans.position_.z = z;
-            Model::SetTransform(hModel_, trans);
-            Model::Draw(hModel_);
-        }
-    }
+	//Model::SetTransform(hModel_, transform_);
+	//Model::Draw(hModel_);
+
+	for (int x = 0; x < 15; x++)
+	{
+		for (int z = 0; z < 15; z++)
+		{
+			for (int y = 0; y < table_[x][z].height + 1; y++)
+			{
+				//table[x][z]からオブジェクトのタイプを取り出して書く！
+				int type = table_[x][z].type;
+				Transform trans;
+				trans.position_.x = x;
+				trans.position_.y = y;
+				trans.position_.z = z;
+				Model::SetTransform(hModel_[type], trans);
+				Model::Draw(hModel_[type]);
+			}
+		}
+	}
 }
 
 //開放
--- Stage.h
+++ Stage.h
@@ -1,11 +1,31 @@
 #pragma once
 #include "Engine/GameObject.h"
 
+
+namespace {
+    const int MODEL_NUM{ 5 };
+    const int XSIZE{ 15 };
+    const int ZSIZE{ 15 };
+    enum BLOCKTYPE
+    {
+        DEFAULT, BRICK, GRASS, SAND, WATER
+    };
+}
+
 //◆◆◆を管理するクラス
 class Stage : public GameObject
 {
-    int hModel_;    //モデル番号
+    int hModel_[MODEL_NUM];    //モデル番号
+    //int table_[XSIZE][ZSIZE];
+    struct
+    {
+        int type;
+        int height;
+    } table_[15][15];
+
 public:
+    void SetBlock(int _x, int _z, BLOCKTYPE _type);
+    void SetBlockHeight(int _x, int _z, int _height);
     //コンストラクタ
     Stage(GameObject* parent);
 
